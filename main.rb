558c3db6ff700fe234b73882a3d9d04245653ad3
Beau Randall <brandall10@gmail.com>
2012-04-02 18:54:49 -0700

    Now deleting from facebook, database kept in parity with facebook

--- main.rb
+++ main.rb
@@ -13,6 +13,7 @@ class Comment
 	include DataMapper::Resource
 	property :id,	Serial
 	property :fbid, String, :required => true
+	property :fbuid, String, :required => true
 	property :comment, String, :required => true
 end
 DataMapper.finalize
@@ -30,29 +31,38 @@ get '/' do
 end
 
 get '/login' do
+	# if user does not have an open facebook session they will be automatically directed to facebook, callback will define what happens after
 	session['oauth'] = Facebook::OAuth.new(APP_ID, APP_SECRET, SITE_URL + 'callback')
+	# must set publish_stream to modify wall feed
 	redirect session['oauth'].url_for_oauth_code(:permissions => "publish_stream")	
 end
 
 get '/logout' do
 	session['oauth'] = nil
 	session['access_token'] = nil
+	session['graph'] = nil
 	redirect '/'
 end
 
 get '/callback' do
 	session['access_token'] = session['oauth'].get_access_token(params[:code])
+	session['graph'] = Facebook::API.new (session['access_token'])
 	redirect '/'
 end
 
 post '/' do
-	@graph = Facebook::API.new (session['access_token'])
-	@graph.put_wall_post(params[:comment][:comment])
+	# must pull back facebook's object id so we can modify afterward, take note to pull from hash
+	# write to both facebook and db to keep parity
+	fbid =  session['graph'].put_wall_post(params[:comment][:comment])["id"]
+	params[:comment][:fbid] = fbid
 	Comment.create params[:comment]
 	redirect '/'
 end
 
 delete '/comment/:id' do
-	Comment.get(params[:id]).destroy
+	comment = Comment.get(params[:id])
+	# delete both from facebook and the database
+	session['graph'].delete_object(comment.fbid)
+	comment.destroy
 	redirect '/'
 end
